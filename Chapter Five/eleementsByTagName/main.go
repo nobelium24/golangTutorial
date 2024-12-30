1075ce0187f0f7ed730336cb25376984c65b904b
nobelium24 <ogunbaja07@gmail.com>
2024-12-30 11:09:12 +0100

    minor update on chapter 5

--- Chapter Five/eleementsByTagName/main.go
+++ Chapter Five/eleementsByTagName/main.go
@@ -2,18 +2,26 @@ package eleementsbytagname
 
 import "golang.org/x/net/html"
 
-func ElementsByName(h *html.Node, name ...string) []*html.Node {
+func ElementsByName(h *html.Node, names ...string) []*html.Node {
 	var nodes []*html.Node
 	if h == nil {
 		return nodes
 	}
-	for _, arg := range name {
-		if h.Type == html.ElementNode && h.Data == arg {
+	var nameMap = make(map[string]struct{})
+	for _, name := range names {
+		nameMap[name] = struct{}{}
+	}
+	// for _, arg := range name {
+	// 	if h.Type == html.ElementNode && h.Data == arg {
+	// 		nodes = append(nodes, h)
+	// 	}
+	// }
+	if h.Type == html.ElementNode {
+		if _, found := nameMap[h.Data]; found {
 			nodes = append(nodes, h)
 		}
 	}
-	nodes = ElementsByName(h.FirstChild, name...)
-	nodes2 := ElementsByName(h.NextSibling, name...)
-	nodes = append(nodes, nodes2...)
+	nodes = append(nodes, ElementsByName(h.FirstChild, names...)...)
+	nodes = append(nodes, ElementsByName(h.NextSibling, names...)...)
 	return nodes
 }
